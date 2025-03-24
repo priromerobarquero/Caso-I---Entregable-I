35cd47ee83b78b8af6601e793a6f00fc496ebd2e
EliPoli64 <168288076+EliPoli64@users.noreply.github.com>
2025-03-23 19:36:56 -0600

    añadida tabla

--- query 4.2.sql
+++ query 4.2.sql
@@ -13,3 +13,22 @@ WHERE
     p.endDate BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 15 DAY)
 ORDER BY 
     p.endDate;
+
+/*
+nombre_completo        | email
+--------------------------------------------
+Juan Pérez             | juan.perez@email.com
+Ana López              | ana.lopez@email.com
+Luis García            | luis.garcia@email.com
+Marta Sánchez          | marta.sanchez@email.com
+Carlos Rodríguez       | carlos.rodriguez@email.com
+Sofía Martínez         | sofia.martinez@email.com
+Pablo González         | pablo.gonzalez@email.com
+Laura Fernández        | laura.fernandez@email.com
+Antonio García         | antonio.garcia@email.com
+Isabel Torres          | isabel.torres@email.com
+Ricardo Herrera        | ricardo.herrera@email.com
+Lucía Ruiz             | lucia.ruiz@email.com
+David Pérez            | david.perez@email.com
+Eva Morales            | eva.morales@email.com
+*/
